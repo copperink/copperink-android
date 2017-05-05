852e491905b1a3329394eaa9d36bbc903da59907
Sheharyar Naseer <hello@sheharyar.me>
2017-05-05 06:02:43 -0400

    Basic Login Fragment

--- app/src/main/java/co/firetools/copperink/controllers/activities/InitialActivity.java
+++ app/src/main/java/co/firetools/copperink/controllers/activities/InitialActivity.java
@@ -1,14 +1,21 @@
 package co.firetools.copperink.controllers.activities;
 
+import android.animation.ObjectAnimator;
 import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
 
 import co.firetools.copperink.R;
 import co.firetools.copperink.services.GlobalService;
 import co.firetools.copperink.services.UserService;
 
 public class InitialActivity extends AppCompatActivity {
+    private final static int SPLASH_WAIT = 2000; // ms
     private Context context;
 
     @Override
@@ -25,9 +32,39 @@ public class InitialActivity extends AppCompatActivity {
         // Load existing user profile
         UserService.loadUser(context);
 
-        if (UserService.userExists())
-            GlobalService.showToast("User Account Exists");
-        else
-            GlobalService.showToast("User Account NOT FOUND");
+
+        // Wait determined time before showing login
+        // or going to next activity
+        new AsyncTask<Void, Void, Void>() {
+            protected Void doInBackground(Void... unused) {
+                SystemClock.sleep(SPLASH_WAIT);
+                return null;
+            }
+
+            protected void onPostExecute(Void unused) {
+                if (UserService.userExists())
+                    GlobalService.showToast("User Account Exists");
+                else
+                    showLoginView();
+            }
+        }.execute();
+    }
+
+
+    /**
+     * Renders the Login view in Fragment
+     */
+    private void showLoginView() {
+        // Initialize Views
+        ImageView   logo  = (ImageView)   findViewById(R.id.logo);
+        FrameLayout login = (FrameLayout) findViewById(R.id.loginView);
+
+        // Animate the Logo
+        ObjectAnimator animator = ObjectAnimator.ofFloat(logo,"y", 150);
+        animator.setDuration(2000);
+        animator.start();
+
+        // Show the Login Fragment
+        login.setVisibility(View.VISIBLE);
     }
 }
---
+++ app/src/main/java/co/firetools/copperink/controllers/fragments/LoginFragment.java
@@ -0,0 +1,27 @@
+package co.firetools.copperink.controllers.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import co.firetools.copperink.R;
+
+
+public class LoginFragment extends Fragment {
+    public LoginFragment() { }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_login, container, false);
+
+        return root;
+    }
+
+}
