52e3f991891031a04b22596b0c489b61ec86adb0
Sheharyar Naseer <hello@sheharyar.me>
2017-05-06 12:49:36 -0400

    ImageResize on Scroll working

--- app/src/main/java/co/firetools/copperink/controllers/activities/MainActivity.java
+++ app/src/main/java/co/firetools/copperink/controllers/activities/MainActivity.java
@@ -1,20 +1,33 @@
 package co.firetools.copperink.controllers.activities;
 
 import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import co.firetools.copperink.R;
 
 public class MainActivity extends AppCompatActivity {
+    AppBarLayout appbar;
+    Toolbar toolbar;
+    ImageView logo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        appbar = (AppBarLayout) findViewById(R.id.app_bar);
+        logo = (ImageView) findViewById(R.id.logo);
         toolbar.setTitle("");
         setSupportActionBar(toolbar);
+
+
+        attachOffsetListener();
+
+
 
 //        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 //        fab.setOnClickListener(new View.OnClickListener() {
@@ -24,5 +37,37 @@ public class MainActivity extends AppCompatActivity {
 //                        .setAction("Action", null).show();
 //            }
 //        });
+    }
+
+
+    /**
+     * Attaches an AppBarOffsetListener to listen for scrolls and
+     * change the logo size in Toolbar accordingly
+     */
+    private void attachOffsetListener() {
+        appbar.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
+            float MAX_LOGO_HEIGHT = getResources().getDimension(R.dimen.logo_height_max);
+            float MIN_LOGO_HEIGHT = getResources().getDimension(R.dimen.logo_height_min);
+
+            @Override
+            public void onOffsetChanged(AppBarLayout appBarLayout, int offset) {
+
+                // Get Maximum Scoll Range
+                int total = appBarLayout.getTotalScrollRange();
+
+                // Calculate scroll ratio from range
+                float scale  = (float) (total + offset) / total;
+
+                // Calculate new height between min-max range
+                float height = MIN_LOGO_HEIGHT + ((MAX_LOGO_HEIGHT - MIN_LOGO_HEIGHT) * scale);
+
+                // Apply new Layout Params
+                logo.requestLayout();
+                ViewGroup.LayoutParams logoParams = logo.getLayoutParams();
+                logoParams.height = (int) height;
+                logo.requestLayout();
+//                logo.setLayoutParams(logoParams);
+            }
+        });
     }
 }
